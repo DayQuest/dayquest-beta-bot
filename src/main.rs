ac24248783d2cb682c19e8f48e874781144bb363
Tiiita <tiiitamail@gmail.com>
2025-02-01 17:39:06 +0100

    Add time mesuarement

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, process::exit};
+use std::{env, process::exit, time::Instant};
 
 use colored::Colorize;
 use config::{Config, BOT_TOKEN_KEY, JWT_TOKEN_KEY};
@@ -6,7 +6,7 @@ use env_logger::{Builder, Env};
 use log::info;
 use poise::{
     serenity_prelude::{
-        self, async_trait, ClientBuilder, EventHandler, GatewayIntents, OnlineStatus, Ready
+        self, async_trait, prelude::TypeMapKey, ClientBuilder, EventHandler, GatewayIntents, OnlineStatus, Ready
     },
     Framework, FrameworkOptions,
 };
@@ -24,6 +24,7 @@ pub type Context<'a> = poise::Context<'a, Data, Error>;
 
 #[tokio::main]
 async fn main() {
+    let start_time = Instant::now();
     ctrlc::set_handler(move || {
         info!("{}", "Stopping bot, Bye :)".on_red());
         exit(0);
@@ -37,24 +38,27 @@ async fn main() {
     if let Ok(_) = dotenv_res {
         info!("Loaded .env file {}", "(development only)".yellow())
     }
-    
+
     let config = config::load();
     let token = env::var(BOT_TOKEN_KEY).expect("Unable to find token in enviroment");
 
     let framework = Framework::builder()
         .options(FrameworkOptions {
-            commands: vec![
-                beta_command::beta(),
-            ],
+            commands: vec![beta_command::beta()],
             ..Default::default()
         })
         .setup(|ctx, _ready, framework| {
             Box::pin(async move {
-                poise::builtins::register_in_guild(ctx, &framework.options().commands, config.guild_id.into()).await?;
+                poise::builtins::register_in_guild(
+                    ctx,
+                    &framework.options().commands,
+                    config.guild_id.into(),
+                )
+                .await?;
 
                 info!("Registered commands globally");
                 Ok(Data {
-                    config, 
+                    config,
                     reqwest: Client::new(),
                     jwt: env::var(JWT_TOKEN_KEY).expect("Failed to get backend jwt from env"),
                 })
@@ -68,7 +72,16 @@ async fn main() {
         .await
         .unwrap();
 
+    {
+        let mut data = client.data.write().await;
+        data.insert::<StartTime>(start_time);
+    }
     client.start().await.unwrap();
+}
+
+struct StartTime;
+impl TypeMapKey for StartTime {
+    type Value = Instant;
 }
 
 struct Handler;
@@ -76,7 +89,8 @@ struct Handler;
 #[async_trait]
 impl EventHandler for Handler {
     async fn ready(&self, ctx: serenity_prelude::Context, ready: Ready) {
-        info!("{} is connected", ready.user.name);
+        let data = ctx.data.read().await;
+        info!("{} is connected, took: {} ms", ready.user.name, data.get::<StartTime>().unwrap().elapsed().as_millis());
         ctx.set_presence(None, OnlineStatus::Online);
     }
 }
